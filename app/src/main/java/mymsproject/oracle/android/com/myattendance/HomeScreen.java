278ae1233fdf24fb484ad2459a31c4bc75ed39bf
Sharath Shankar <SharathBabu.Shankar@synchronoss.com>
2018-07-04 21:02:10 +0530

    Inital check-in

---
+++ app/src/main/java/mymsproject/oracle/android/com/myattendance/HomeScreen.java
@@ -0,0 +1,13 @@
+package mymsproject.oracle.android.com.myattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
